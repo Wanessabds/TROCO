99651bddfa51c94d2d3c33160ad08186a6bb839f
Wanessbds <wanessa.nessap@gmail.com>
2023-03-31 21:58:17 -0300

    TROCO

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+decimal valorCompra, valorPago, valorTroco;
+
+Console.WriteLine("--- NOTA DA COMPRA ---\n");
+
+Console.Write("Digite o valor da compra (R$): ");
+valorCompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Valor pago pelo cliente (R$): ");
+valorPago = Convert.ToDecimal(Console.ReadLine());
+
+valorTroco = valorPago - valorCompra;
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine($"\n Valor do Troco: {valorTroco:C}");
